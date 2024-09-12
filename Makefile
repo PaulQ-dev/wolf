727d17b366655dd9edc20d6bde8d45ecd7658aaf
PaulQ-dev <paulq-dev@mail.ru>
2024-09-12 22:39:21 +0300

    updated map wramework & fullscreen is live!

--- Makefile
+++ Makefile
@@ -21,12 +21,15 @@ clean_obj:
 	@echo "Object files cleaned"
 clean: clean_bin clean_obj
 
+obj/wolf/map.o: src/wolf/map.cpp
+	@echo "Compiling src/wolf/map.cpp"
+	@$(GXX) -c -o $@ $< -Isrc/wolf -lSDL2
 obj/wolf/main.o: src/wolf/main.cpp
 	@echo "Compiling src/wolf/main.cpp"
 	@$(GXX) -c -o $@ $< -Isrc/wolf -lSDL2
 obj/wolf/window.o: src/wolf/window.cpp
 	@echo "Compiling src/wolf/window.cpp"
 	@$(GXX) -c -o $@ $< -Isrc/wolf -lSDL2
-wolf: obj/wolf/main.o obj/wolf/window.o
+wolf: obj/wolf/map.o obj/wolf/main.o obj/wolf/window.o
 	@echo "Combining wolf"
 	@$(GXX) -o bin/wolf $^ -Isrc/wolf -lSDL2
--- src/wolf/main.cpp
+++ src/wolf/main.cpp
@@ -23,7 +23,8 @@ int sgn(float a){
     return a > 0 ? 1 : a < 0 ? -1 : 0;
 }
 
-int map[24*24];
+map_t* map;
+int* mapData;
 
 typedef struct{
     float x;
@@ -31,8 +32,8 @@ typedef struct{
     float a;
 } Player;
 
-#define winW 1120
-#define winH 630
+int winW = 1120;
+int winH = 630;
 #define plrS 4
 #define rotS 3.0
 #define movS 0.4
@@ -42,21 +43,33 @@ Player* plr;
 bool quit;
 float spd, spdA;
 int plrSX, plrSY;
-bool mapM;
 
-
+void prepMap(){
+    mapData = (int*)calloc(map->w*map->h, sizeof(int));
+    memcpy(mapData, map->map, map->w*map->h*sizeof(int));
+    plr->x = mapS*(map->stX+.5);
+    plr->y = mapS*(map->stY+.5);
+    plr->a = 90;
+}
+void prepMap(int mapN){ //mapN is ingored for now
+    map = createMap0();
+    prepMap();
+}
 
 void init(){
     SDL_Init(SDL_INIT_VIDEO);
     win = create("Window",winW,winH);
+    SDL_SetWindowFullscreen(win->win,SDL_WINDOW_FULLSCREEN_DESKTOP);
+    SDL_GetWindowSizeInPixels(win->win, &(win->w), &(win->h));
+    winH = win->h;
+    winW = win->w;
     plr = (Player*)malloc(sizeof(Player));
-    memcpy(map, mapAr, mapW*mapH*sizeof(int));
-    plr->x = mapS*(plrStX+.5);
-    plr->y = mapS*(plrStY+.5);
-    plr->a = 90;
+    prepMap(0);
 }
 
 void cleanup(){
+    mapDispose(map);
+    free(mapData);
     cleanup(win);
     SDL_Quit();
 }
@@ -94,12 +107,9 @@ void handleEvents(){
                 spdY = mapS/2*dsin(plr->a);
                 nextSX = (plr->x+spdX+plrS/2*sgn(spdX))/mapS;
                 nextSY = (plr->y-spdY-plrS/2*sgn(spdY))/mapS;
-                if(map[nextSY*mapW+nextSX] == 2){
-                    map[nextSY*mapW+nextSX] = 0;
+                if(mapData[nextSY*map->w+nextSX] == 2){
+                    mapData[nextSY*map->w+nextSX] = 0;
                 }
-                break;
-            case SDLK_m:
-                mapM = true;
                 break;
             default:
                 break;
@@ -118,38 +128,32 @@ void handleEvents(){
             case SDLK_a:
                 spdA = 0;
                 break;
-            case SDLK_m:
-                mapM = false;
-                break;
             case SDLK_p:
-                memcpy(map, mapAr, mapW*mapH*sizeof(int));
-                plr->x = mapS*(plrStX+.5);
-                plr->y = mapS*(plrStY+.5);
-                plr->a = 90;
+                prepMap();
             default:
                 break;
             }
         }
     }
 }
-float drawRay(Window* win, Player* plr, float a, Uint8* cl){
+float castRay(Window* win, Player* plr, float a, Uint8* cl){
     float dx = dcos(a);
     float dy = dsin(a);
     float x = plr->x;
     float y = plr->y;
-    while(map[(int)y/mapS*mapW+(int)x/mapS]==0){
+    while(mapData[(int)y/mapS*map->w+(int)x/mapS]==0){
         x+=dx*.1;
         y-=dy*.1;
     }
-    int val = map[(int)y/mapS*mapW+(int)x/mapS];
+    int val = mapData[(int)y/mapS*map->w+(int)x/mapS];
     if(val == 1){
-        cl[0] = 128;
-        cl[1] = 128;
-        cl[2] = 96;
+        cl[0] = map->clW_r;
+        cl[1] = map->clW_g;
+        cl[2] = map->clW_b;
     }else if(val == 2){
-        cl[0] = 128;
-        cl[1] = 128;
-        cl[2] = 128;
+        cl[0] = map->clD_r;
+        cl[1] = map->clD_g;
+        cl[2] = map->clD_b;
     }
     return sqrtf((x-plr->x)*(x-plr->x)+(y-plr->y)*(y-plr->y));
 }
@@ -163,49 +167,68 @@ void drawWallSlice(Window* win, int x, float h, float d, Uint8* cl){
     SDL_RenderDrawLine(win->ren, x,y,x,y+h);
 }
 void draw3d(){
+    int renW = winW-minimapN*minimapS;
     for(int i = 0; i < winH/2; i++){
-        Uint8 cl = (int)(((winH-i)/(float)winH)*48);
-        SDL_SetRenderDrawColor(win->ren,0,0,cl,255);
-        SDL_RenderDrawLine(win->ren, 0, i, winW-1, i);
+        Uint8 clR = (int)(((winH-i)/(float)winH)*map->clC_r);
+        Uint8 clG = (int)(((winH-i)/(float)winH)*map->clC_g);
+        Uint8 clB = (int)(((winH-i)/(float)winH)*map->clC_b);
+        SDL_SetRenderDrawColor(win->ren,clR,clG,clB,255);
+        SDL_RenderDrawLine(win->ren, 0, i, winW-1-(winW-renW), i);
     }
     for(int i = 0; i < winH/2; i++){
-        Uint8 cl = (int)(((i+winH/2)/(float)winH)*64);
-        SDL_SetRenderDrawColor(win->ren,cl,cl,cl,255);
-        SDL_RenderDrawLine(win->ren, 0, i+winH/2, winW-1, i+winH/2);
+        Uint8 clR = (int)(((i+winH/2)/(float)winH)*map->clF_r);
+        Uint8 clG = (int)(((i+winH/2)/(float)winH)*map->clF_g);
+        Uint8 clB = (int)(((i+winH/2)/(float)winH)*map->clF_b);
+        SDL_SetRenderDrawColor(win->ren,clR,clG,clB,255);
+        SDL_RenderDrawLine(win->ren, 0, i+winH/2, winW-1-(winW-renW), i+winH/2);
     }
-    int rays = winW;
-    float da = 70.0/(float)rays;
-    for(int i = 0; i < rays; i++){
+    float da = 70.0/(float)renW;
+    for(int i = 0; i < renW; i++){
         float a = (plr->a)-35+(float)i*da;
         Uint8 cl[3];
-        float d = drawRay(win, plr, a, cl);
+        float d = castRay(win, plr, a, cl);
         d = d * dcos(a-plr->a);
-        drawWallSlice(win,winW-i-2,((float)winH)/2/d*mapS,d, cl);
+        drawWallSlice(win,winW-i-1-(winW-renW),((float)winH)/2/d*mapS,d, cl);
     }
 }
-void draw2d(){
+void drawMap(){
     int x = winW-minimapN*minimapS;
     SDL_Rect rect;
     rect.w = minimapS;
     rect.h = minimapS;
     for(int i = 0; i < minimapN; i++) for(int j = 0; j < minimapN; j++){
         int jS = j-(minimapN/2)+plrSY, iS = i-(minimapN/2)+plrSX;
-        if(jS < 0 || iS < 0 || jS > mapH || iS > mapW || map[(jS)*mapW+(iS)]) SDL_SetRenderDrawColor(win->ren,255,255,255,255);
-        else SDL_SetRenderDrawColor(win->ren,0,0,0,255);
+        if(jS < 0 || iS < 0 || jS >= map->h || iS >= map->w || mapData[(jS)*map->w+(iS)])
+             SDL_SetRenderDrawColor(win->ren,map->clW_r,map->clW_g,map->clW_b,255);
+        else SDL_SetRenderDrawColor(win->ren,map->clF_r,map->clF_g,map->clF_b,255);
         rect.x = x+i*minimapS;
         rect.y = j*minimapS;
         SDL_RenderFillRect(win->ren, &rect);
     }
     SDL_SetRenderDrawColor(win->ren,255,0,0,255);
     int plPos = minimapN*minimapS/2;
-    SDL_Point lines[3];
+    SDL_Point lines[5];
     lines[0].x = x+round(plPos+minimapPlS*dcos(plr->a+120));
     lines[0].y = round(plPos-minimapPlS*dsin(plr->a+120));
     lines[1].x = x+round(plPos+minimapPlS*dcos(plr->a));
     lines[1].y = round(plPos-minimapPlS*dsin(plr->a));
     lines[2].x = x+round(plPos+minimapPlS*dcos(plr->a-120));
     lines[2].y = round(plPos-minimapPlS*dsin(plr->a-120));
-    SDL_RenderDrawLines(win->ren, lines, 3);
+    lines[3].x = x+round(plPos);
+    lines[3].y = round(plPos);
+    lines[4].x = lines[0].x;
+    lines[4].y = lines[0].y;
+    SDL_RenderDrawLines(win->ren, lines, 5);
+}
+void drawStatBar(){
+    SDL_SetRenderDrawColor(win->ren,map->clW_r,map->clW_g,map->clW_b,255);
+    SDL_Rect rect;
+    rect.y = minimapN*minimapS;
+    rect.x = winW-rect.y;
+    rect.w = rect.y;
+    rect.h = winH-rect.y;
+    SDL_SetRenderDrawColor(win->ren,map->clW_r,map->clW_g,map->clW_b,255);
+    SDL_RenderFillRect(win->ren, &rect);
 }
 
 void display(){
@@ -217,20 +240,21 @@ void display(){
         float spdY = spd*dsin(plr->a);
         int nextSX = (plr->x+spdX+plrS/2*sgn(spdX))/mapS;
         int nextSY = (plr->y-spdY-plrS/2*sgn(spdY))/mapS;
-        if(map[plrSY*mapW+nextSX]==0) {
+        if(mapData[plrSY*map->w+nextSX]==0) {
             plr->x+=spdX/2;
         }
-        if(map[nextSY*mapW+plrSX]==0){
+        if(mapData[nextSY*map->w+plrSX]==0){
             plr->y-=spdY/2;
         }
-        if(map[nextSY*mapW+nextSX]==0){
+        if(mapData[nextSY*map->w+nextSX]==0){
             plr->y-=spdY/2;
             plr->x+=spdX/2;
         }
         plrSX = (int)(plr->x)/mapS;
         plrSY = (int)(plr->y)/mapS;
         draw3d();
-        if (mapM) draw2d();
+        drawMap();
+        drawStatBar();
         render();
     }
 }
